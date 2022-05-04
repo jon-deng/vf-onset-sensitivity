c469ddd4849ab16723b76ae7b3d0dd3ceb695fd2
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-05-04 10:46:17 -0400

    Refactor set_props for inv analysis stress test

--- main_inv_stress_test_lsa.py
+++ main_inv_stress_test_lsa.py
@@ -22,16 +22,8 @@ import libhopf
 # Range of psub to test for Hopf bifurcation
 PSUBS = np.arange(100, 1300, 100)*10
 
-def set_props(res_dyn, emod_cov, emod_bod):
-    # Get the cover/body layer DOFs
-    if isinstance(res_dyn, libhopf.HopfModel):
-        _forms = res_dyn.res.solid.forms
-    else:
-        _forms = res_dyn.solid.forms
-    celllabel_to_dofs = meshutils.process_celllabel_to_dofs_from_forms(_forms, _forms['fspace.scalar'])
-
+def set_props(props, celllabel_to_dofs, emod_cov, emod_bod):
     # Set any constant properties
-    props = res_dyn.props
     props = setup.set_constant_props(props, celllabel_to_dofs, res_dyn)
 
     # Set cover and body layer properties
@@ -39,11 +31,14 @@ def set_props(res_dyn, emod_cov, emod_bod):
     dofs_bod = np.array(celllabel_to_dofs['body'], dtype=np.int32)
     props['emod'].array[dofs_cov] = emod_cov
     props['emod'].array[dofs_bod] = emod_bod
-
-    res_dyn.set_props(props)
+    return props
 
 def run_lsa(f, res_dyn, emod_cov, emod_bod):
-    set_props(res_dyn, emod_cov, emod_bod)
+    # Get the cover/body layer DOFs
+    _forms = res_dyn.solid.forms
+    celllabel_to_dofs = meshutils.process_celllabel_to_dofs_from_forms(_forms, _forms['fspace.scalar'])
+    props = set_props(res_dyn.props, celllabel_to_dofs, emod_cov, emod_bod)
+    res_dyn.set_props(props)
 
     for group_name in ['eigvec_real', 'eigvec_imag', 'fixedpoint']:
         h5utils.create_resizable_block_vector_group(
@@ -68,7 +63,11 @@ def run_lsa(f, res_dyn, emod_cov, emod_bod):
             h5utils.append_block_vector_to_group(f[group_name], eigvec)
 
 def run_solve_hopf(f, res_hopf, emod_cov, emod_bod):
-    set_props(res_hopf, emod_cov, emod_bod)
+    # Set the cover/body layer properties
+    _forms = res_hopf.res.solid.forms
+    celllabel_to_dofs = meshutils.process_celllabel_to_dofs_from_forms(_forms, _forms['fspace.scalar'])
+    props = set_props(res_hopf.props, celllabel_to_dofs, emod_cov, emod_bod)
+    res_hopf.set_props(props)
 
     # Read the max real eigenvalue information from the LSA to determine if Hopf
     # bifurcations occur and a good starting point
@@ -80,23 +79,26 @@ def run_solve_hopf(f, res_hopf, emod_cov, emod_bod):
 
     is_hopf_bif = [(w2 > 0 and w1 <=0) for w1, w2 in zip(omegas_real[:-1], omegas_real[1:])]
     # breakpoint()
-    # if is_hopf_bif.count(True) == 0:
-    #     print(f"Case {lsa_fname} has no Hopf bifurcations")
-    # else:
-    #     idx_hopf = is_hopf_bif.index(True)
+    if is_hopf_bif.count(True) == 0:
+        print(f"Case {lsa_fname} has no Hopf bifurcations")
+        print(f"Real eigenvalue components are {omegas_real}")
+    else:
+        idx_hopf = is_hopf_bif.index(True)
+        print(f"Case {lsa_fname} has a Hopf bifurcation between {PSUBS[idx_hopf]} {PSUBS[idx_hopf+1]} dPa")
+        print(f"Real eigenvalue components are {omegas_real}")
 
-    #     xhopf_0 = libhopf.gen_hopf_initial_guess(
-    #         res_hopf,
-    #         res_hopf.E_MODE,
-    #         ([omegas_real[idx_hopf], omegas_real[idx_hopf+1]])
-    #     )
-    #     xhopf_n, info = libhopf.solve_hopf_newton(res_hopf, xhopf_0)
+        xhopf_0 = libhopf.gen_hopf_initial_guess(
+            res_hopf,
+            ([PSUBS[idx_hopf]], [PSUBS[idx_hopf+1]]),
+            ([omegas_real[idx_hopf]], [omegas_real[idx_hopf+1]])
+        )
+        xhopf_n, info = libhopf.solve_hopf_newton(res_hopf, xhopf_0)
 
-    #     fname = f'Hopf_ecov{emod_cov:.2e}_ebody{emod_bod:.2e}'
-    #     fpath = f'out/{fname}.h5'
-    #     with h5py.File(fpath, mode='w') as f:
-    #         h5utils.create_resizable_block_vector_group(f, xhopf_n.labels, xhopf_n.bshape)
-    #         h5utils.append_block_vector_to_group(f, xhopf_n)
+        fname = f'Hopf_ecov{emod_cov:.2e}_ebody{emod_bod:.2e}'
+        fpath = f'out/{fname}.h5'
+        with h5py.File(fpath, mode='w') as f:
+            h5utils.create_resizable_block_vector_group(f, xhopf_n.labels, xhopf_n.bshape)
+            h5utils.append_block_vector_to_group(f, xhopf_n)
 
 
 if __name__ == '__main__':
@@ -114,7 +116,7 @@ if __name__ == '__main__':
     # print(EMODS)
 
     for ii, emod_bod in enumerate(EMODS):
-        for emod_cov in EMODS[ii:]:
+        for emod_cov in EMODS[:ii+1]:
             fname = f'LSA_ecov{emod_cov:.2e}_ebody{emod_bod:.2e}'
             fpath = f'out/stress_test/{fname}.h5'
 
@@ -127,7 +129,7 @@ if __name__ == '__main__':
                 print(f"File {fpath} already exists")
 
     for ii, emod_bod in enumerate(EMODS):
-        for emod_cov in EMODS[ii:]:
+        for emod_cov in EMODS[:ii+1]:
             fname = f'Hopf_ecov{emod_cov:.2e}_ebody{emod_bod:.2e}'
             fpath = f'out/stress_test/{fname}.h5'
 
