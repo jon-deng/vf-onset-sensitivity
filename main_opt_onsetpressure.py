10de90d1b307fbfbb0735c46b7a8afb932671990
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-05-06 18:46:19 -0400

    Add exploration of smoothing effect to onset pressure optimization

--- main_opt_onsetpressure.py
+++ main_opt_onsetpressure.py
@@ -2,17 +2,18 @@
 Solve a simple test optimization problem
 """
 
+import sys
 import os.path as path
 from pprint import pprint
 
 import h5py
+import numpy as np
 from scipy import optimize
 from blockarray import blockvec  as bvec
 
 import libhopf
 import libfunctionals as libfuncs
 from test_libhopf import setup_hopf_state
-
 
 if __name__ == '__main__':
     # Load the Hopf system
@@ -24,38 +25,52 @@ if __name__ == '__main__':
     # Load the measurement glottal width error + reduced gradient
     func_onset_pressure = libfuncs.OnsetPressureFunctional(hopf)
     func_onset_frequency = libfuncs.AbsOnsetFrequencyFunctional(hopf)
-    func = func_onset_pressure + 1000.0*(func_onset_frequency-float(abs(xhopf['omega'][0])))**2
+    func_egrad_norm = libfuncs.ModulusGradientNormSqr(hopf)
 
-    redu_grad = libhopf.ReducedGradient(func, hopf)
-
-    # Create the objective function and gradient needed for optimization
-    x0 = bvec.concatenate_vec([redu_grad.props.copy(), redu_grad.camp.copy()])
-    x0['amp'].set(1.0)
-
-    ## Optimization
-    opt_options = {
-        'disp': 99,
-        'maxiter': 100,
-        'ftol': 0.0,
-        'maxls': 100
-    }
-    def opt_callback(xk):
-        print("In callback")
-
-    with h5py.File("out/opt_hist.h5", mode='w') as f:
-        grad_manager = libhopf.OptGradManager(redu_grad, f)
-        # opt_res = optimize.minimize(
-        #     grad_manager.grad, x0.to_mono_ndarray(),
-        #     method='L-BFGS-B',
-        #     jac=True,
-        #     options=opt_options,
-        #     callback=opt_callback
-        # )
-
-        opt_res = optimize.fmin_l_bfgs_b(
-            grad_manager.grad, x0.to_mono_ndarray(),
-            factr=0.0, iprint=99
+    # `alpha` is a weight on the gradient smoothing of modulus
+    # A characteristic gradient of 1kPa/1cm over a volume of 1cm^2 has a
+    # functional value of 1e8 Ba^2*cm^2 (cgs unit)
+    # 'unit' value of alpha should then start around 1e-8
+    for alpha in 10**np.array([-np.inf]+[-10, -8, -6, -5.5, -5.0, -4.5, -4, -2, 0]):
+        func = (
+            func_onset_pressure
+            + 1000.0*(func_onset_frequency-float(abs(xhopf['omega'][0])))**2
+            + alpha*func_egrad_norm
         )
 
-    pprint(opt_res)
+        redu_grad = libhopf.ReducedGradient(func, hopf)
 
+        # Create the objective function and gradient needed for optimization
+        x0 = bvec.concatenate_vec([props0.copy(), redu_grad.camp.copy()])
+        x0[-2:] = 0.0 # these correspond to complex amp. parameters
+
+        ## Optimization
+        opt_options = {
+            'disp': 99,
+            'maxiter': 50,
+            'ftol': 0.0,
+            'maxls': 100
+        }
+        def opt_callback(xk):
+            print("In callback")
+
+        fpath = f"out/optimize_onset_pressure/opt_hist_alpha{alpha:.2e}.h5"
+        if not path.isfile(fpath):
+            with h5py.File(fpath, mode='w') as f:
+                grad_manager = libhopf.OptGradManager(redu_grad, f)
+                # opt_res = optimize.minimize(
+                #     grad_manager.grad, x0.to_mono_ndarray(),
+                #     method='L-BFGS-B',
+                #     jac=True,
+                #     options=opt_options,
+                #     callback=opt_callback
+                # )
+
+                opt_res = optimize.fmin_l_bfgs_b(
+                    grad_manager.grad, x0.to_mono_ndarray(),
+                    factr=0.0, iprint=99
+                )
+
+            pprint(opt_res)
+        else:
+            print(f"File {fpath} already exists.")
