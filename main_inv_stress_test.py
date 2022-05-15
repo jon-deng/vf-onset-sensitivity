923e32836188d6f7a4781e9bd6198a3cdcc34b97
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-05-15 11:32:32 -0400

    Refactor main_inv_stress_test.py to allow multi-processing

--- main_inv_stress_test.py
+++ main_inv_stress_test.py
@@ -5,9 +5,10 @@ The stress test cases consist of all combinations of body and cover moduli where
 the moduli range from 2.5 to 10 in steps of 2.5 (kPa).
 """
 
+import argparse
+import multiprocessing
 import os.path as path
 import itertools
-import warnings
 from pprint import pprint
 
 import petsc4py
@@ -34,6 +35,7 @@ EMODS = np.arange(2.5, 12.5+2.5, 2.5) * 1e3*10
 
 EMODS = np.arange(2.5, 10.5+0.5, 0.5) * 1e3*10
 EMODS = np.arange(5.0, 7.0, 0.5) * 1e3 * 10
+EMODS = np.array([2.5]) * 1e3*10
 
 ## The models are not pickalble so have to be outside for multi-processing
 mesh_name = 'BC-dcov5.00e-02-cl1.00'
@@ -64,17 +66,21 @@ def set_props(props, celllabel_to_dofs, emod_cov, emod_bod):
 
     return props
 
-def run_lsa(f, emod_cov, emod_bod):
+def _ignore_existing_path(func):
+    def dec_func(fpath, *args, **kwargs):
+        if not path.isfile(fpath):
+            return func(fpath, *args, **kwargs)
+        else:
+            print(f"File {fpath} already exists.")
+    return dec_func
+
+@_ignore_existing_path
+def run_lsa(fpath, emod_cov, emod_bod):
     # Get the cover/body layer DOFs
     _forms = RES_DYN.solid.forms
     celllabel_to_dofs = meshutils.process_celllabel_to_dofs_from_forms(_forms, _forms['fspace.scalar'])
     props = set_props(RES_DYN.props, celllabel_to_dofs, emod_cov, emod_bod)
     RES_DYN.set_props(props)
-
-    for group_name in ['eigvec_real', 'eigvec_imag', 'fixedpoint']:
-        bh5utils.create_resizable_block_vector_group(
-            f.require_group(group_name), RES_DYN.state.labels, RES_DYN.state.bshape
-        )
 
     eigs_info = [libhopf.solve_least_stable_mode(RES_DYN, psub) for psub in PSUBS]
 
@@ -84,18 +90,24 @@ def run_lsa(f, emod_cov, emod_bod):
     eigvecs_imag = [eiginfo[2] for eiginfo in eigs_info]
     xfps = [eiginfo[3] for eiginfo in eigs_info]
 
-    f['psub'] = PSUBS
-    f['omega_real'] = np.array(omegas_real)
-    f['omega_imag'] = np.array(omegas_imag)
-    for group_name, eigvecs in zip(
-            ['eigvec_real', 'eigvec_imag', 'fixedpoint'],
-            [eigvecs_real, eigvecs_imag, xfps]
-        ):
-        for eigvec in eigvecs:
-            bh5utils.append_block_vector_to_group(f[group_name], eigvec)
+    with h5py.File(fpath, mode='a') as f:
+        f['psub'] = PSUBS
+        f['omega_real'] = np.array(omegas_real)
+        f['omega_imag'] = np.array(omegas_imag)
+
+        for group_name, eigvecs in zip(
+                ['eigvec_real', 'eigvec_imag', 'fixedpoint'],
+                [eigvecs_real, eigvecs_imag, xfps]
+            ):
+            bh5utils.create_resizable_block_vector_group(
+                f.require_group(group_name), RES_DYN.state.labels, RES_DYN.state.bshape
+            )
+            for eigvec in eigvecs:
+                bh5utils.append_block_vector_to_group(f[group_name], eigvec)
     return PSUBS, omegas_real
 
-def run_solve_hopf(f, emod_cov, emod_bod):
+@_ignore_existing_path
+def run_solve_hopf(fpath, emod_cov, emod_bod):
     # Set the cover/body layer properties
     _forms = RES_HOPF.res.solid.forms
     celllabel_to_dofs = meshutils.process_celllabel_to_dofs_from_forms(_forms, _forms['fspace.scalar'])
@@ -126,13 +138,15 @@ def run_solve_hopf(f, emod_cov, emod_bod):
         )
         xhopf_n, info = libhopf.solve_hopf_newton(RES_HOPF, xhopf_0)
 
-        bh5utils.create_resizable_block_vector_group(f.require_group('state'), xhopf_n.labels, xhopf_n.bshape)
-        bh5utils.append_block_vector_to_group(f['state'], xhopf_n)
+        with h5py.File(fpath, mode='a') as f:
+            bh5utils.create_resizable_block_vector_group(f.require_group('state'), xhopf_n.labels, xhopf_n.bshape)
+            bh5utils.append_block_vector_to_group(f['state'], xhopf_n)
 
-        bh5utils.create_resizable_block_vector_group(f.require_group('props'), props.labels, props.bshape)
-        bh5utils.append_block_vector_to_group(f['props'], props)
+            bh5utils.create_resizable_block_vector_group(f.require_group('props'), props.labels, props.bshape)
+            bh5utils.append_block_vector_to_group(f['props'], props)
 
-def run_large_amp_model(f, emod_cov, emod_bod):
+@_ignore_existing_path
+def run_large_amp_model(fpath, emod_cov, emod_bod):
     """
     Run a non-linear/large amplitude (transient) oscillation model
     """
@@ -143,9 +157,9 @@ def run_large_amp_model(f, emod_cov, emod_bod):
     RES_LAMP.set_props(props)
 
     # Load the onset pressure from the Hopf simulation
-    fname = f'Hopf_ecov{emod_cov:.2e}_ebody{emod_bod:.2e}'
-    fpath = f'out/stress_test/{fname}.h5'
-    with h5py.File(fpath, mode='r') as f_hopf:
+    h_fname = f'Hopf_ecov{emod_cov:.2e}_ebody{emod_bod:.2e}'
+    h_fpath = f'out/stress_test/{h_fname}.h5'
+    with h5py.File(h_fpath, mode='r') as f_hopf:
         if 'state' in f_hopf:
             xhopf = bh5utils.read_block_vector_from_group(f_hopf['state'])
             ponset = xhopf['psub'][0]
@@ -168,12 +182,12 @@ def run_large_amp_model(f, emod_cov, emod_bod):
 
         control = RES_LAMP.control.copy()
         control['psub'][:] = ponset + 100.0*10
-
-        forward.integrate(RES_LAMP, f, ini_state, [control], RES_LAMP.props, times, use_tqdm=True)
+        with sf.StateFile(RES_LAMP, fpath, mode='a') as f:
+            forward.integrate(RES_LAMP, f, ini_state, [control], RES_LAMP.props, times, use_tqdm=True)
     else:
         print(f"Skipping large amplitude simulation of {fname} because no Hopf bifurcation is detected")
 
-def postproc_gw(f, emods_cov, emods_bod):
+def postproc_gw(fpath, emods_cov, emods_bod):
     """
     Compute glottal width and time data from large amp. simulations
     """
@@ -191,9 +205,11 @@ def postproc_gw(f, emods_cov, emods_bod):
     ]
     in_paths = [f'out/stress_test/{name}.h5' for name in in_names]
 
-    return postprocutils.postprocess_case_to_signal(f, in_paths, RES_LAMP, signal_to_proc)
+    with h5py.File(fpath, mode='a') as f:
+        return postprocutils.postprocess_case_to_signal(f, in_paths, RES_LAMP, signal_to_proc)
 
-def run_inv_opt(f, emod_cov, emod_bod, gw_ref, omega_ref, alpha=0.0, opt_options=None):
+@_ignore_existing_path
+def run_inv_opt(fpath, emod_cov, emod_bod, gw_ref, omega_ref, alpha=0.0, opt_options=None):
 
     ## Set the Hopf system properties
     _forms = RES_HOPF.res.solid.forms
@@ -216,10 +232,6 @@ def run_inv_opt(f, emod_cov, emod_bod, gw_ref, omega_ref, alpha=0.0, opt_options
     func = func_gw_err + func_freq_err + func_egrad_norm
     redu_grad = libhopf.ReducedGradient(func, RES_HOPF)
 
-    ## Record the reference gw and freq
-    f['gw_ref'] = gw_ref
-    f['omega_ref'] = omega_ref
-
     redu_grad.set_props(props)
 
     ## Run the optimizer
@@ -230,7 +242,7 @@ def run_inv_opt(f, emod_cov, emod_bod, gw_ref, omega_ref, alpha=0.0, opt_options
     def opt_callback(xk):
         print("In callback")
 
-    scale = 1e3*np.ones((x0.msize,))
+    scale = 1e3*np.ones(x0.mshape)
     scale[-1] = 1.0 # make sure the phase argument has no scaling
     def assem_sc_grad(sc_x, scale, assem_grad):
         x = sc_x * scale
@@ -240,20 +252,38 @@ def run_inv_opt(f, emod_cov, emod_bod, gw_ref, omega_ref, alpha=0.0, opt_options
         sc_grad = scale * grad
         return sc_obj, sc_grad
 
-    # with h5py.File(f"out/opt_hist_{fname}.h5", mode='w') as f:
-    grad_manager = libhopf.OptGradManager(redu_grad, f)
-    opt_res = optimize.minimize(
-        assem_sc_grad,
-        x0.to_mono_ndarray()/scale,
-        args=(scale, grad_manager.grad),
-        method='L-BFGS-B',
-        jac=True,
-        options=opt_options,
-        callback=opt_callback
-    )
+    with h5py.File(fpath, mode='a') as f:
+         ## Record the reference gw and freq
+        f['gw_ref'] = gw_ref
+        f['omega_ref'] = omega_ref
 
-    pprint(opt_res)
+        grad_manager = libhopf.OptGradManager(redu_grad, f)
+        opt_res = optimize.minimize(
+            assem_sc_grad,
+            x0.to_mono_ndarray()/scale,
+            args=(scale, grad_manager.grad),
+            method='L-BFGS-B',
+            jac=True,
+            options=opt_options,
+            callback=opt_callback
+        )
 
+        pprint(opt_res)
+
+def _run_inv_opt(emod_cov, emod_bod, alpha, gt_fname):
+    fname = f'OptInv_emod{emod_cov:.2e}_ebody{emod_bod:.2e}_alpha_{alpha:.2e}_gt{gt_fname}'
+    fpath = f'out/stress_test/{fname}.h5'
+    print(f'Optimizing case {fname}')
+    try:
+        run_inv_opt(
+            fpath,
+            emod_cov, emod_bod,
+            gw_ref, omega_ref,
+            alpha=alpha,
+            opt_options=opt_options
+        )
+    except petsc4py.PETSc.Error as err:
+        print(f"Case failed with error {err}")
 
 def segment_last_period(y, dt):
     # Segment the final cycle from the glottal width and estimate an uncertainty
@@ -281,7 +311,12 @@ def optimize_comp_amp(func_gw_err):
     return camp0
 
 
+
 if __name__ == '__main__' :
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--numproc', type=int, default=1)
+    args = parser.parse_args()
+
     emods = [
         (ecov, ebod) for ecov, ebod in itertools.product(EMODS, EMODS)
         if ecov <= ebod
@@ -292,89 +327,80 @@ if __name__ == '__main__' :
     for emod_cov, emod_bod in zip(emods_cov, emods_bod):
         fname = f'LSA_ecov{emod_cov:.2e}_ebody{emod_bod:.2e}'
         fpath = f'out/stress_test/{fname}.h5'
+        ans = run_lsa(fpath, emod_cov, emod_bod)
 
-        if not path.isfile(fpath):
-            with h5py.File(fpath, mode='w') as f:
-                psubs, omegas_real = run_lsa(f, emod_cov, emod_bod)
-
-                if max(omegas_real) >= 0.0 and min(omegas_real) < 0.0:
-                    is_hopf = [a < 0 and b >=0 for a, b in zip(omegas_real[:-1], omegas_real[1:])]
-                    ii = is_hopf.index(True)
-                    print(f"{fname} has a Hopf bifurcation between {psubs[ii]:.2f} Ba and {psubs[ii+1]:.2f} Ba")
-                else:
-                    print(f"{fname} does not have a Hopf bifurcation between {psubs[0]:.2f} Ba and {psubs[-1]:.2f} Ba")
-        else:
-            print(f"File {fpath} already exists")
+        if ans is not None:
+            psubs, omegas_real = ans
+            if max(omegas_real) >= 0.0 and min(omegas_real) < 0.0:
+                is_hopf = [a < 0 and b >=0 for a, b in zip(omegas_real[:-1], omegas_real[1:])]
+                ii = is_hopf.index(True)
+                print(f"{fname} has a Hopf bifurcation between {psubs[ii]:.2f} Ba and {psubs[ii+1]:.2f} Ba")
+            else:
+                print(f"{fname} does not have a Hopf bifurcation between {psubs[0]:.2f} Ba and {psubs[-1]:.2f} Ba")
 
     ## Solve the Hopf bifurcation system for each case
     for emod_cov, emod_bod in zip(emods_cov, emods_bod):
         fname = f'Hopf_ecov{emod_cov:.2e}_ebody{emod_bod:.2e}'
         fpath = f'out/stress_test/{fname}.h5'
-
-        if not path.isfile(fpath):
-            with h5py.File(fpath, mode='w') as f:
-                run_solve_hopf(f, emod_cov, emod_bod)
-        else:
-            print(f"File {fpath} already exists")
+        run_solve_hopf(fpath, emod_cov, emod_bod)
 
     ## Run a transient simulation for each Hopf bifurcation
     for emod_cov, emod_bod in zip(emods_cov, emods_bod):
         fname = f'LargeAmp_ecov{emod_cov:.2e}_ebody{emod_bod:.2e}'
         fpath = f'out/stress_test/{fname}.h5'
-
-        if not path.isfile(fpath):
-            with sf.StateFile(RES_LAMP, fpath, mode='w') as f:
-                run_large_amp_model(f, emod_cov, emod_bod)
-        else:
-            print(f"File {fpath} already exists")
+        run_large_amp_model(fpath, emod_cov, emod_bod)
 
     ## Post process the glottal width and time from each transient simulation
     fpath = 'out/stress_test/signals.h5'
-    # emods = [(ecov, ebod) for ecov, ebod in itertools.product(EMODS, EMODS) if ecov <= ebod]
-    # emods_cov = [e[0] for e in emods]
-    # emods_bod = [e[1] for e in emods]
-    with h5py.File(fpath, mode='a') as f:
-        SIGNALS = postproc_gw(fpath, emods_cov, emods_bod)
+    SIGNALS = postproc_gw(fpath, emods_cov, emods_bod)
 
     ## Run the inverse analysis studies
     # determine cover/body combinations that self-oscillate
-    emods = [
+    _emods = [
         (ecov, ebod) for ecov, ebod in zip(emods_cov, emods_bod)
         if len(SIGNALS[f'LargeAmp_ecov{ecov:.2e}_ebody{ebod:.2e}/gw']) != 0
     ]
-    for emod_cov, emod_bod in zip(emods_cov, emods_bod):
-        # Load the reference glottal width and omega
-        _name = f'LargeAmp_ecov{emod_cov:.2e}_ebody{emod_bod:.2e}'
-        gw_ref = SIGNALS[f'{_name}/gw']
-        t_ref = SIGNALS[f'{_name}/time']
+    _emods_cov = [x[0] for x in _emods]
+    _emods_bod = [x[1] for x in _emods]
+    ## Load the reference glottal width and omega
+    for emod_cov, emod_bod in zip(_emods_cov, _emods_bod):
+        gt_fname = f'LargeAmp_ecov{emod_cov:.2e}_ebody{emod_bod:.2e}'
+        gw_ref = SIGNALS[f'{gt_fname}/gw']
+        t_ref = SIGNALS[f'{gt_fname}/time']
         dt = t_ref[1]-t_ref[0]
 
         # Segment the last period
         gw_ref, omega_ref = segment_last_period(gw_ref, dt)
 
-        # Try to optimize to the target data from all starting points
+        ## Try to optimize to the target data from all starting points
         opt_options = {
             'disp': 99,
             'maxiter': 150
         }
-        for emod_cov, emod_bod in zip(emods_cov, emods_bod):
-            alpha = 0.0
-            for alpha in 10**np.array([-np.inf] + [-6, -4, -2]):
-                fname = f'OptInv_emod{emod_cov:.2e}_ebody{emod_bod:.2e}_alpha_{alpha:.2e}_gt{_name}'
-                fpath = f'out/stress_test/{fname}.h5'
-                print(f'Optimizing case {fname}')
-                if path.isfile(fpath):
-                    print(f"File {fpath} already exists")
-                else:
-                    try:
-                        with h5py.File(fpath, mode='w') as f:
-                            run_inv_opt(
-                                f,
-                                emod_cov, emod_bod,
-                                gw_ref, omega_ref,
-                                alpha=alpha,
-                                opt_options=opt_options
-                            )
-                    except petsc4py.PETSc.Error as err:
-                        print(f"Case failed with error {err}")
+        alphas = 10**np.array([-np.inf] + [-6, -4, -2])
 
+        # for emod_cov, emod_bod in zip(_emods_cov, _emods_bod):
+        #     alpha = 0.0
+        #     for alpha in alphas:
+        #         fname = f'OptInv_emod{emod_cov:.2e}_ebody{emod_bod:.2e}_alpha_{alpha:.2e}_gt{_name}'
+        #         fpath = f'out/stress_test/{fname}.h5'
+        #         print(f'Optimizing case {fname}')
+        #         try:
+        #             run_inv_opt(
+        #                 fpath,
+        #                 emod_cov, emod_bod,
+        #                 gw_ref, omega_ref,
+        #                 alpha=alpha,
+        #                 opt_options=opt_options
+        #             )
+        #         except petsc4py.PETSc.Error as err:
+        #             print(f"Case failed with error {err}")
+
+
+        _args = [(emod[0], emod[1], alpha) for emod, alpha in itertools.product(_emods, alphas)]
+
+        def _run(ecov, ebod, alpha):
+            return _run_inv_opt(ecov, ebod, alpha, gt_fname)
+
+        with multiprocessing.Pool(args.numproc) as p:
+            p.starmap(_run, _args)
