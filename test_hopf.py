a86a6e53c197b0fc049f90db5f7b179c4621f5f4
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-08-20 13:51:45 -0400

    doc: Add better comments to test_hopf.py

--- test_hopf.py
+++ test_hopf.py
@@ -1,10 +1,16 @@
 """
-Testing code for finding hopf bifurcations of coupled FE VF models
+Test the Hopf system residual derivatives
+
+This module contains tests to check correctness of the `libhopf.Hopf` residual
+sensitivity functions.
+Correctness is tested by checking the residual sensitivity against finite
+differences computed from the residual.
 """
+
 from os import path
 import numpy as np
 
-from femvf.meshutils import process_celllabel_to_dofs_from_forms
+# from femvf.meshutils import process_celllabel_to_dofs_from_forms
 import blockarray.linalg as bla
 
 from libsetup import set_default_props, load_hopf
@@ -56,6 +62,8 @@ def test_assem_dres_dstate(hopf, state0, dstate):
 
     def hopf_res(x):
         hopf.set_state(x)
+        res = hopf.assem_res()
+        hopf.apply_dirichlet_bvec(res)
         return hopf.assem_res()
 
     def hopf_jac(x):
@@ -81,45 +89,55 @@ def test_assem_dres_dprops(hopf, props0, dprops):
 
 
 if __name__ == '__main__':
+    ## Load the Hopf model to test
     mesh_name = 'BC-dcov5.00e-02-cl1.00'
     mesh_path = path.join('./mesh', mesh_name+'.msh')
-    model, res, dres = load_hopf(mesh_path, sep_method='smoothmin', sep_vert_label='separation-inf')
-
-    ## Set model properties
-    # get the scalar DOFs associated with the cover/body layers
-    region_to_dofs = process_celllabel_to_dofs_from_forms(
-        res.solid.forms,
-        res.solid.forms['fspace.scalar']
+    model, res, dres = load_hopf(
+        mesh_path, sep_method='smoothmin', sep_vert_label='separation-inf'
     )
+
+    ## Set the Hopf model properties
+    # get the scalar DOFs associated with the cover/body layers
+    # region_to_dofs = process_celllabel_to_dofs_from_forms(
+    #     res.solid.forms,
+    #     res.solid.forms['fspace.scalar']
+    # )
 
     props = model.props.copy()
     props = set_default_props(props, res.solid.forms['mesh.mesh'])
 
+    ## Test sensitivities of the Hopf model residual w.r.t each block of
+    ## `Hopf.state`
+
+    # Set the linearization point
     (state_labels,
      mode_real_labels,
      mode_imag_labels,
      psub_labels,
      omega_labels) = model.labels_hopf_components
 
-    ## Test dF/dstate
     x0 = model.state.copy()
     x0[mode_real_labels] = 1.0
     x0[mode_imag_labels] = 1.0
-    x0[psub_labels[0]] = PSUB
-    x0[omega_labels[0]] = 1.0
+    x0[psub_labels] = PSUB
+    x0[omega_labels] = 1.0
     model.apply_dirichlet_bvec(x0)
-    model.set_state(x0)
+    # model.set_state(x0)
 
+    # Test the residual derivative in multiple directions
+    # Each direction corresponds to a block of `Hopf.state`
+    dx = x0.copy()
     for label in model.state.labels[0]:
         print(f"\n -- Checking Hopf jacobian along {label} --")
-        dx = x0.copy()
         dx[:] = 0
         dx[label] = 1e-5
         model.apply_dirichlet_bvec(dx)
 
         test_assem_dres_dstate(model, x0, dx)
 
-    ## Test dF/dprops
+    ## Test sensitivities of the Hopf model residual w.r.t each block of
+    ## `Hopf.props`
+    # TODO: This only checks one block `Hopf.props` right now
     props0 = props.copy()
     dprops = props0.copy()
     dprops[:] = 0.0
