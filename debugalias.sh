40a8bae66b04c65814f7a077fa1aa206a2d91320
Jonathan Deng <jonathan.j.deng@gmail.com>
2023-05-31 18:15:58 -0400

    misc: Add aliases to allow debugging

---
+++ debugalias.sh
@@ -0,0 +1,2 @@
+alias testnew='cd ~/code/vf-fem && git checkout debug-sensitivity-results-afterchange && cd ~/code/vf-onset-sensitivity && git checkout debug-sensitivity-results-afterchange'
+alias testold='cd ~/code/vf-fem && git checkout debug-sensitivity-results-beforechange && cd ~/code/vf-experiments/hopf-bifurcation-femodel && git checkout debug-sensitivity-results-beforechange'
